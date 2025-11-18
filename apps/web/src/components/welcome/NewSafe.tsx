4ff4793fc0c0d7a59c87ddd0cb417b6ae94366ae
VaibhavAureus <vaibhav@aureus.money>
2025-11-18 13:13:19 +0700

    Added more branding on he landing page

--- apps/web/src/components/welcome/NewSafe.tsx
+++ apps/web/src/components/welcome/NewSafe.tsx
@@ -10,13 +10,13 @@ const NewSafe = () => {
   return (
     <div className={css.loginPage}>
       <div className={css.leftSide}>
-        <div className={css.logoContainer}>
+        {/* <div className={css.logoContainer}>
           {BRAND_LOGO ? (
             <img src={BRAND_LOGO} alt={BRAND_NAME} className={css.logo} style={{ height: '40px', width: 'auto' }} />
           ) : (
             <Typography variant="h4" fontWeight={700}>{BRAND_NAME}</Typography>
           )}
-        </div>
+        </div> */}
         <div className={css.loginContainer}>
           <WelcomeLogin />
         </div>
@@ -27,9 +27,50 @@ const NewSafe = () => {
         <div className={css.rightContent}>
           <Typography className={css.label}>FOR ORGANIZATIONS AND POWER USERS</Typography>
           <Typography className={css.mainTitle}>Own your assets onchain securely</Typography>
+          <Typography className={css.subtitle}>
+            A dedicated Aureus Safe interface to custody your onchain assets with multisig security, built for teams
+            and power users.
+          </Typography>
         </div>
         <div className={css.mockupImageContainer}>
-          <img src="/images/welcome/safe-mockup.png" alt="Safe interface mockup" className={css.mockupImage} />
+          <svg
+            className={css.mockupImage}
+            viewBox="0 0 424 697"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-label="Aureus"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M53.1387 354.043L35.5879 411.788L386.997 410.153L368.91 352.574L53.1387 354.043Z"
+              fill="white"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M17.5508 469.624L0 527.37L422.685 525.403L404.598 467.824L17.5508 469.624Z"
+              fill="white"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M209.34 0L1.21997 685.02L61.5729 684.739L210.25 195.567L363.471 683.335L423.905 683.054L209.34 0Z"
+              fill="white"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M5.58032 696.097L1.21997 685.02L61.5729 684.739L5.58032 696.097Z"
+              fill="white"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M423.905 683.054L419.637 694.2L363.471 683.335L423.905 683.054Z"
+              fill="white"
+            />
+          </svg>
         </div>
       </div>
     </div>
